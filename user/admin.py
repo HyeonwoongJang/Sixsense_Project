139eae667ee23a56ce59c6372b5b8ab9e426d6e1
HyeonwoongJang <hyeonwoongjang01@gmail.com>
2023-09-11 10:27:46 +0900

    make user model and add to admin site

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from user.models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+# UserModel(사용자 모델)을 관리자 인터페이스에서 관리해줍니다.
+admin.site.register(User, UserAdmin)
+
+# @admin.register(User) 
+class UserAdmin(admin.ModelAdmin):
+  list_display = ["id", "username", "email", "nickname", "created_at", "updated_at"]
--- user/models.py
+++ user/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+"""
+userid : 로그인 시 사용되는 사용자의 ID
+password : 로그인 시 사용되는 사용자의 비밀번호
+email : 사용자의 Email
+img : 사용자의 프로필 사진
+nickname : 사용자의 활동 이름
+created_at : 사용자의 계정이 생성된 시간
+updated_at : 사용자의 계정 정보가 수정된 시간
+"""
+
+class User(AbstractUser):
+  nickname = models.CharField(max_length=20, null=False)
+  image = models.ImageField(blank=True, null=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  
