8a476434210e574bbd14c99fc450d3e93b64a39b
HyeonwoongJang <hyeonwoongjang01@gmail.com>
2023-09-11 11:27:21 +0900

    signup feature done

--- user/views.py
+++ user/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from user.models import User
+
+def signup(request):
+  if request.method == 'GET':
+    return render(request, 'user/signup.html')
+  elif request.method == 'POST':
+      username = request.POST['username'],
+      password = request.POST['password'],
+      password2 = request.POST['password2'],
+      email = request.POST['email']
+      img = request.POST['img']
+      nickname = request.POST['nickname']
+      if password == password2 :
+        User.objects.create_user(username=username, password=password, password2=password2, email=email, img=img, nickname=nickname)
+      else :
+        return HttpResponse('password does not match')
+  else:
+    return HttpResponse("Invalid request method", status=405)
