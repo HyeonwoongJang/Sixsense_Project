46b98d42345e29745a39d1a845120f82a8f4c869
HyeonwoongJang <hyeonwoongjang01@gmail.com>
2023-09-17 21:39:58 +0900

    count feature done

--- MyLord/views.py
+++ MyLord/views.py
@@ -20,11 +20,9 @@ def myhome(request, user_id):
         page_user = User.objects.get(id=user_id)
         my_posts = Post.objects.filter(
             username_id=user_id).order_by('-created_at')
-        follow = request.user in page_user.follower.all()
         my_book_marks = page_user.rn_book_mark.all()
         my_like_posts = Like.objects.filter(user=user_id)
-        followers = page_user.follower.all().count()
-        return render(request, 'myhome.html', {'tem_my_posts': my_posts, 'tem_page_user': page_user, 'tem_follow': follow, 'tem_followers':followers, 'tem_my_bookmarks': my_book_marks, 'tem_my_like_posts': my_like_posts})
+        return render(request, 'myhome.html', {'tem_my_posts': my_posts, 'tem_page_user': page_user, 'tem_my_bookmarks': my_book_marks, 'tem_my_like_posts': my_like_posts})
     else:
         return HttpResponse("invalid request method", status=405)
 
