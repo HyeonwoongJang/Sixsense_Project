6effaa3ad76cecf26b62c49a475c70533c527955
HyeonwoongJang <hyeonwoongjang01@gmail.com>
2023-09-17 21:02:30 +0900

    count feature done 2

--- MyLord/views.py
+++ MyLord/views.py
@@ -21,8 +21,7 @@ def myhome(request, user_id):
         my_posts = Post.objects.filter(
             username_id=user_id).order_by('-created_at')
         follow = request.user in page_user.follower.all()
-        me = page_user
-        my_book_marks = me.rn_book_mark.all()
+        my_book_marks = page_user.rn_book_mark.all()
         my_like_posts = Like.objects.filter(user=user_id)
         followers = page_user.follower.all().count()
         return render(request, 'myhome.html', {'tem_my_posts': my_posts, 'tem_page_user': page_user, 'tem_follow': follow, 'tem_followers':followers, 'tem_my_bookmarks': my_book_marks, 'tem_my_like_posts': my_like_posts})
